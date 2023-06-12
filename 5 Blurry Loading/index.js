d95ffd8727352e04259fa7c9ce9e8c263797c434
Miller1999 <millerjavierarias@gmail.com>
2023-06-11 23:05:25 -0500

    Terminado dia 5

---
+++ 5 Blurry Loading/index.js
@@ -0,0 +1,20 @@
+const texto = document.getElementById("texto")
+const imagen = document.getElementById("imagen")
+
+let carga  = 0
+
+let int = setInterval(loading,30)
+
+function loading(){
+    carga++
+    if(carga > 99){
+        clearInterval(int)
+    }
+    texto.innerHTML = `${carga}%`
+    texto.style.opacity = scale(carga,0,100,0,1)
+    imagen.style.filter = `blur(${scale(carga,0,100,30,0)}px)`
+}
+
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
